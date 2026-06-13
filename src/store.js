d6f63c886082ae279f30424f08ec7d23b7560723
TahaSrn <tahaahmadi0403@gmail.com>
2026-06-13 15:46:48 +0330

    adding local storage

--- src/store.js
+++ src/store.js
@@ -2,11 +2,35 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
 import cartReducer from "./features/cart/cartSlice";
 
+function loadState() {
+  try {
+    const serialized = localStorage.getItem("pizzaAppState");
+    return serialized ? JSON.parse(serialized) : undefined;
+  } catch {
+    return undefined;
+  }
+}
+
+function saveState(state) {
+  try {
+    localStorage.setItem(
+      "pizzaAppState",
+      JSON.stringify({
+        user: { username: state.user.username },
+        cart: { cart: state.cart.cart },
+      }),
+    );
+  } catch {}
+}
+
 const store = configureStore({
   reducer: {
     user: userReducer,
     cart: cartReducer,
   },
+  preloadedState: loadState(),
 });
+
+store.subscribe(() => saveState(store.getState()));
 
 export default store;
